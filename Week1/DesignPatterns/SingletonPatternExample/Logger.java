05ef8616a7dda4aa9099f575df6bc17584204096
Utsah Sinha <utsah9@gmail.com>
2026-06-29 17:00:53 +0530

    Week1: Ex1 - Singleton Pattern complete

---
+++ Week1/DesignPatterns/SingletonPatternExample/Logger.java
@@ -0,0 +1,30 @@
+public class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger initialized.");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First message");
+        logger2.log("Second message");
+
+        // Proves both variables point to same object
+        System.out.println("Same instance? " + (logger1 == logger2));
+    }
+}
