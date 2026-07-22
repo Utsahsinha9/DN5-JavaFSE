22f457310852dbb0ec664d528ef8952432b5cdb8
Utsah Sinha <utsah9@gmail.com>
2026-07-22 18:17:03 +0530

    Week2: Ex9 - Creating a Spring Boot Application complete

---
+++ Week2/SpringCore/Ex9-SpringBootApp/src/main/java/com/dn5/springcore/Ex9SpringBootAppApplication.java
@@ -0,0 +1,11 @@
+package com.dn5.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex9SpringBootAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Ex9SpringBootAppApplication.class, args);
+    }
+}
